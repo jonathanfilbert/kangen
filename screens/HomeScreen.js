d62064120c6ff84a57e33d434ff38554d36e06ef
Jonathanfilbert <jonathan.lisyanto@bpkpenabur.sch.id>
2020-06-23 18:50:49 +0700

    big bang

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,150 +1,191 @@
-import * as WebBrowser from 'expo-web-browser';
-import * as React from 'react';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import React, { useEffect, useState } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { Notifications } from "expo";
+import * as Permissions from "expo-permissions";
+import Constants from "expo-constants";
+import { Clipboard } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import AsyncStorage from "@react-native-community/async-storage";
 
-import { MonoText } from '../components/StyledText';
+const HomeScreen = () => {
+  const [expoToken, setExpoToken] = useState("");
+  const [friendToken, setFriendToken] = useState("");
+  const registerForPushNotificationsAsync = async () => {
+    if (Constants.isDevice) {
+      const { status: existingStatus } = await Permissions.getAsync(
+        Permissions.NOTIFICATIONS
+      );
+      let finalStatus = existingStatus;
+      if (existingStatus !== "granted") {
+        const { status } = await Permissions.askAsync(
+          Permissions.NOTIFICATIONS
+        );
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        alert("Failed to get push token for push notification!");
+        return;
+      }
+      token = await Notifications.getExpoPushTokenAsync();
+      setExpoToken(token.split("[")[1].split("]")[0]);
+    } else {
+      alert("Must use physical device for Push Notifications");
+    }
 
-export default function HomeScreen() {
+    if (Platform.OS === "android") {
+      Notifications.createChannelAndroidAsync("default", {
+        name: "default",
+        sound: true,
+        priority: "max",
+        vibrate: [0, 250, 250, 250],
+      });
+    }
+  };
+
+  const handleCopy = () => {
+    Clipboard.setString(expoToken);
+    alert("Copied to clipboard!");
+  };
+
+  const handleSaveToken = () => {
+    console.log(friendToken);
+    AsyncStorage.setItem("friendToken", friendToken);
+    alert("SAVED");
+  };
+
+  useEffect(() => {
+    registerForPushNotificationsAsync();
+  }, []);
   return (
     <View style={styles.container}>
-      <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
-          />
-        </View>
-
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
-
-          <Text style={styles.getStartedText}>Open up the code for this screen:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
-
-          <Text style={styles.getStartedText}>
-            Change any of the text, save the file, and your app will automatically reload.
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          alignSelf: "stretch",
+          paddingHorizontal: 5,
+        }}
+      >
+        <Text>Your Profile Code is</Text>
+        <Text style={{ fontWeight: "bold", fontSize: 20, textAlign: "center" }}>
+          {expoToken === "" ? "Loading..." : expoToken}
+        </Text>
+      </View>
+      <TouchableOpacity style={{ marginTop: 20 }} onPress={handleCopy}>
+        <View style={{ backgroundColor: "black", borderRadius: 5 }}>
+          <Text
+            style={{
+              color: "white",
+              paddingHorizontal: 20,
+              paddingVertical: 5,
+              fontSize: 20,
+              textAlignVertical: "center",
+              textAlign: "center",
+            }}
+          >
+            COPY TO CLIPBOARD
           </Text>
         </View>
-
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-        <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>navigation/BottomTabNavigator.js</MonoText>
-        </View>
+      </TouchableOpacity>
+      <View style={{ marginTop: 40 }}>
+        <Text>Insert your partner's code</Text>
+        <TextInput
+          style={{ height: 40, borderColor: "gray", borderBottomWidth: 2 }}
+          onChangeText={(text) => setFriendToken(text)}
+        />
+        <TouchableOpacity
+          style={{ marginTop: 20 }}
+          onPress={() => handleSaveToken()}
+        >
+          <View style={{ backgroundColor: "black", borderRadius: 5 }}>
+            <Text
+              style={{
+                color: "white",
+                paddingHorizontal: 20,
+                paddingVertical: 5,
+                fontSize: 20,
+                textAlignVertical: "center",
+                textAlign: "center",
+              }}
+            >
+              INSERT
+            </Text>
+          </View>
+        </TouchableOpacity>
       </View>
     </View>
   );
-}
+};
 
 HomeScreen.navigationOptions = {
   header: null,
 };
 
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
-
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use useful development
-        tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
-    );
-  }
-}
-
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/workflow/development-mode/');
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/get-started/create-a-new-app/#making-your-first-change'
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
+    display: "flex",
+    paddingTop: 50,
+    paddingHorizontal: 20,
   },
   developmentModeText: {
     marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
+    color: "rgba(0,0,0,0.4)",
     fontSize: 14,
     lineHeight: 19,
-    textAlign: 'center',
+    textAlign: "center",
   },
   contentContainer: {
     paddingTop: 30,
   },
   welcomeContainer: {
-    alignItems: 'center',
+    alignItems: "center",
     marginTop: 10,
     marginBottom: 20,
   },
   welcomeImage: {
     width: 100,
     height: 80,
-    resizeMode: 'contain',
+    resizeMode: "contain",
     marginTop: 3,
     marginLeft: -10,
   },
   getStartedContainer: {
-    alignItems: 'center',
+    alignItems: "center",
     marginHorizontal: 50,
   },
   homeScreenFilename: {
     marginVertical: 7,
   },
   codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
+    color: "rgba(96,100,109, 0.8)",
   },
   codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
+    backgroundColor: "rgba(0,0,0,0.05)",
     borderRadius: 3,
     paddingHorizontal: 4,
   },
   getStartedText: {
     fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
+    color: "rgba(96,100,109, 1)",
     lineHeight: 24,
-    textAlign: 'center',
+    textAlign: "center",
   },
   tabBarInfoContainer: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 0,
     left: 0,
     right: 0,
     ...Platform.select({
       ios: {
-        shadowColor: 'black',
+        shadowColor: "black",
         shadowOffset: { width: 0, height: -3 },
         shadowOpacity: 0.1,
         shadowRadius: 3,
@@ -153,27 +194,29 @@ const styles = StyleSheet.create({
         elevation: 20,
       },
     }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
+    alignItems: "center",
+    backgroundColor: "#fbfbfb",
     paddingVertical: 20,
   },
   tabBarInfoText: {
     fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
+    color: "rgba(96,100,109, 1)",
+    textAlign: "center",
   },
   navigationFilename: {
     marginTop: 5,
   },
   helpContainer: {
     marginTop: 15,
-    alignItems: 'center',
+    alignItems: "center",
   },
   helpLink: {
     paddingVertical: 15,
   },
   helpLinkText: {
     fontSize: 14,
-    color: '#2e78b7',
+    color: "#2e78b7",
   },
 });
+
+export default HomeScreen;
--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,53 +1,227 @@
-import { Ionicons } from '@expo/vector-icons';
-import * as WebBrowser from 'expo-web-browser';
-import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { RectButton, ScrollView } from 'react-native-gesture-handler';
+import { Ionicons } from "@expo/vector-icons";
+import * as WebBrowser from "expo-web-browser";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import AsyncStorage from "@react-native-community/async-storage";
 
-export default function LinksScreen() {
+const LinkScreen = () => {
+  const [friendToken, setFriendToken] = useState("");
+  const [name, setName] = useState("");
+  const [isOptionsVisible, setIsOptionsVisible] = useState(false);
+  useEffect(() => {
+    AsyncStorage.getItem("friendToken").then(
+      (res) => res && setFriendToken(res)
+    );
+    AsyncStorage.getItem("myName").then((res) => res && setName(res));
+  }, []);
+
+  const handleChangeName = (name) => {
+    AsyncStorage.setItem("myName", name).then(() => {
+      setName(name);
+    });
+  };
+
+  const message = {
+    missing: `${name} is missing you...`,
+    call: `${name} mau call NOWW`,
+    pr: `${name} MAU DIAJARIN PR WOII SUSAHH 😭 `,
+    laper: `${name} IS HUNGRYYY, gojekin dong hehhe`,
+    sendirian: `${name} mau dijemputtt`,
+    jalan: `${name} mau jalan sama u ni`,
+    line: `BUKA LINE BEGOOOOOOOO`,
+  };
+
+  const getMessageByType = (type) => {
+    const messageTypes = [
+      "missing",
+      "call",
+      "pr",
+      "laper",
+      "sendirian",
+      "jalan",
+      "line",
+    ];
+    if (type !== "random") {
+      return message[type];
+    } else {
+      let randomType =
+        messageTypes[Math.floor(Math.random() * messageTypes.length)];
+      return message[randomType];
+    }
+  };
+
+  const handleSendNotification = (messagetype) => {
+    console.log(friendToken);
+    let response = fetch("https://exp.host/--/api/v2/push/send", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        to: `ExponentPushToken[${friendToken}]`,
+        sound: "default",
+        title: `${name}`,
+        body: getMessageByType(messagetype).toString(),
+        _displayInForeground: true,
+      }),
+    });
+  };
+
   return (
-    <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-      <OptionButton
-        icon="md-school"
-        label="Read the Expo documentation"
-        onPress={() => WebBrowser.openBrowserAsync('https://docs.expo.io')}
-      />
-
-      <OptionButton
-        icon="md-compass"
-        label="Read the React Navigation documentation"
-        onPress={() => WebBrowser.openBrowserAsync('https://reactnavigation.org')}
-      />
-
-      <OptionButton
-        icon="ios-chatboxes"
-        label="Ask a question on the forums"
-        onPress={() => WebBrowser.openBrowserAsync('https://forums.expo.io')}
-        isLastOption
-      />
+    <ScrollView style={styles.container}>
+      <View>
+        <Text style={{ fontSize: 20 }}>Your name</Text>
+        <TextInput
+          value={name}
+          style={{
+            height: 40,
+            borderColor: "gray",
+            borderBottomWidth: 2,
+            fontSize: 20,
+          }}
+          onChangeText={(text) => setName(text)}
+          onEndEditing={(e) => handleChangeName(e.nativeEvent.text)}
+        />
+      </View>
+      <View style={{ marginTop: 20 }}>
+        <TouchableOpacity
+          style={{ marginTop: 20 }}
+          onPress={() => handleSendNotification("missing")}
+        >
+          <View style={{ backgroundColor: "black", borderRadius: 5 }}>
+            <Text
+              style={{
+                color: "white",
+                paddingHorizontal: 20,
+                paddingVertical: 5,
+                fontSize: 20,
+                textAlignVertical: "center",
+                textAlign: "center",
+              }}
+            >
+              MISSING YOU 😍
+            </Text>
+          </View>
+        </TouchableOpacity>
+        {isOptionsVisible && (
+          <View>
+            <TouchableOpacity
+              style={{ marginTop: 20 }}
+              onPress={() => handleSendNotification("call")}
+            >
+              <View style={{ backgroundColor: "black", borderRadius: 5 }}>
+                <Text
+                  style={{
+                    color: "white",
+                    paddingHorizontal: 20,
+                    paddingVertical: 5,
+                    fontSize: 20,
+                    textAlignVertical: "center",
+                    textAlign: "center",
+                  }}
+                >
+                  WANNA CALL 📞
+                </Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{ marginTop: 20 }}
+              onPress={() => handleSendNotification("pr")}
+            >
+              <View style={{ backgroundColor: "black", borderRadius: 5 }}>
+                <Text
+                  style={{
+                    color: "white",
+                    paddingHorizontal: 20,
+                    paddingVertical: 5,
+                    fontSize: 20,
+                    textAlignVertical: "center",
+                    textAlign: "center",
+                  }}
+                >
+                  NEED HELP PR 📚
+                </Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{ marginTop: 20 }}
+              onPress={() => handleSendNotification("laper")}
+            >
+              <View style={{ backgroundColor: "black", borderRadius: 5 }}>
+                <Text
+                  style={{
+                    color: "white",
+                    paddingHorizontal: 20,
+                    paddingVertical: 5,
+                    fontSize: 20,
+                    textAlignVertical: "center",
+                    textAlign: "center",
+                  }}
+                >
+                  HANGRY 🍔
+                </Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{ marginTop: 20 }}
+              onPress={() => handleSendNotification("random")}
+            >
+              <View style={{ backgroundColor: "black", borderRadius: 5 }}>
+                <Text
+                  style={{
+                    color: "white",
+                    paddingHorizontal: 20,
+                    paddingVertical: 5,
+                    fontSize: 20,
+                    textAlignVertical: "center",
+                    textAlign: "center",
+                  }}
+                >
+                  SURPRISE HIM/HER 😏
+                </Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        )}
+        <TouchableOpacity
+          style={{ marginTop: 20 }}
+          onPress={() => setIsOptionsVisible((prev) => !prev)}
+        >
+          <View style={{ backgroundColor: "salmon", borderRadius: 100 }}>
+            <Text
+              style={{
+                color: "white",
+                paddingHorizontal: 20,
+                paddingVertical: 5,
+                fontSize: 20,
+                textAlignVertical: "center",
+                textAlign: "center",
+              }}
+            >
+              {isOptionsVisible ? "HIDE" : "SHOW"} PRESETS ✨
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </ScrollView>
   );
-}
-
-function OptionButton({ icon, label, onPress, isLastOption }) {
-  return (
-    <RectButton style={[styles.option, isLastOption && styles.lastOption]} onPress={onPress}>
-      <View style={{ flexDirection: 'row' }}>
-        <View style={styles.optionIconContainer}>
-          <Ionicons name={icon} size={22} color="rgba(0,0,0,0.35)" />
-        </View>
-        <View style={styles.optionTextContainer}>
-          <Text style={styles.optionText}>{label}</Text>
-        </View>
-      </View>
-    </RectButton>
-  );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fafafa',
+    display: "flex",
+    backgroundColor: "#fafafa",
+    paddingHorizontal: 20,
+    paddingTop: 50,
   },
   contentContainer: {
     paddingTop: 15,
@@ -56,19 +230,21 @@ const styles = StyleSheet.create({
     marginRight: 12,
   },
   option: {
-    backgroundColor: '#fdfdfd',
+    backgroundColor: "#fdfdfd",
     paddingHorizontal: 15,
     paddingVertical: 15,
     borderWidth: StyleSheet.hairlineWidth,
     borderBottomWidth: 0,
-    borderColor: '#ededed',
+    borderColor: "#ededed",
   },
   lastOption: {
     borderBottomWidth: StyleSheet.hairlineWidth,
   },
   optionText: {
     fontSize: 15,
-    alignSelf: 'flex-start',
+    alignSelf: "flex-start",
     marginTop: 1,
   },
 });
+
+export default LinkScreen;
